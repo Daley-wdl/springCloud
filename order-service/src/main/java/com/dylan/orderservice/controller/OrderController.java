88d9a3b5d078cee9d40fde31b816243a0a235d45
wudelong <wudelong@baijiahulian.com>
2021-06-07 10:17:52 +0800

    tool&&leetcode

--- order-service/src/main/java/com/dylan/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/dylan/orderservice/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class OrderController {
     @PostMapping("list")
     public User list(String id) {
         return orderService.getUser(id);
+    }
+
+    @GetMapping("testTomcat")
+    public String testTomcat(String name) {
+        return "hello " + name;
     }
 }
--- order-service/src/main/java/com/dylan/orderservice/controller/ProcessController.java
+++ order-service/src/main/java/com/dylan/orderservice/controller/ProcessController.java
@@ -1,5 +1,8 @@
 package com.dylan.orderservice.controller;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.common.collect.Lists;
 
 import java.util.*;
@@ -15,19 +18,61 @@ public class ProcessController {
 
 
         int[] nums = {3,2,5,4, 6,7,8};
-        System.out.println(partitionDisjoint(nums));
+//        System.out.println(partitionDisjoint(nums));
 
-        Stack<Integer> stack = new Stack<>();
-        Integer peek = stack.peek();
+        /**
+         *               5
+         *              / \
+         *             4   8
+         *            /   / \
+         *           11  13  4
+         *          /  \    / \
+         *         7    2  5   1
+         */
 
+        TreeNode root = new TreeNode(5, new TreeNode(4), new TreeNode(8));
+        root.left.left = new TreeNode(11, new TreeNode(7), new TreeNode(2));
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4, new TreeNode(5), new TreeNode(1));
 
-        List<Integer> objects = Lists.newArrayList();
+        List<List<Integer>> result = Lists.newArrayList();
+        if(0 == 0) {
+            System.out.println(JSONUtil.toJsonStr(result));
+        }
+        process(root, 0, new ArrayList<>(), result);
+        System.out.println(JSONUtil.toJsonStr(result));
+    }
 
+    public static void process(TreeNode root, int current, List<Integer> list, List<List<Integer>> result){
 
-        Deque<Object> deque = new LinkedList<>();
-        deque.peek();
+        if(current < 0) {
+            return;
+        }
 
+        if(current == 0) {
+            System.out.println(JSONUtil.toJsonStr(list));
+            result.add(new ArrayList<>(list));
+            return;
+        }
 
+        if(root == null){
+            return;
+        }
+
+        if(current - root.val >= 0) {
+            list.add(root.val);
+            /**
+             * root current left right
+             * 2
+             */
+            if(root.left == null && root.right == null){
+                process(root.left, current - root.val, list, result);
+            } else {
+                process(root.left, current - root.val, list, result);
+                process(root.right, current - root.val, list, result);
+            }
+            list.remove(list.size() -1);
+        }
     }
 
 
@@ -47,5 +92,18 @@ public class ProcessController {
         }
         return ans + 1; // we have to return length not idx so ans+1.
     }
+
+       public static class TreeNode {
+           int val;
+           TreeNode left;
+           TreeNode right;
+           TreeNode() {}
+           TreeNode(int val) { this.val = val; }
+           TreeNode(int val, TreeNode left, TreeNode right) {
+               this.val = val;
+               this.left = left;
+               this.right = right;
+           }
+        }
 
 }
